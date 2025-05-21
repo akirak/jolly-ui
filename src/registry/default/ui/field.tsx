b29358e9b6b322ae1938328c671544b8385c65d7
Akira Komamura <akira.komamura@gmail.com>
2025-05-22 00:41:34 +0900

    fix(components): Improve support for the disabled state

--- src/registry/default/ui/field.tsx
+++ src/registry/default/ui/field.tsx
@@ -18,7 +18,7 @@ import { cn } from "@/lib/utils"
 const labelVariants = cva([
   "text-sm font-medium leading-none",
   /* Disabled */
-  "data-[disabled]:cursor-not-allowed data-[disabled]:opacity-70",
+  "group-data-[disabled]:cursor-not-allowed group-data-[disabled]:opacity-70",
   /* Invalid */
   "group-data-[invalid]:text-destructive",
 ])
