dabfc834b9fa7d0a3e66e5c2413da387083b2bcd
Akira Komamura <akira.komamura@gmail.com>
2025-05-22 01:01:02 +0900

    chore: Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  outputs =
+    { nixpkgs, ... }:
+    let
+      eachSystem =
+        f:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.bun
+          ];
+        };
+      });
+    };
+}
